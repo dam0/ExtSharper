c35bae72e9051ba2ade3094adc3ee72879cba63b
Taha Hachana <tahahachana@live.com>
2013-02-25 10:45:07 +0100

    first commit

---
+++ ExtSharper/AssemblyInfo.fs
@@ -0,0 +1,14 @@
+﻿namespace ExtSharper
+
+open System.Reflection
+
+module AssemblyInfo =
+
+    [<AssemblyCopyright("Copyright Taha Hachana 2012")>]
+    [<AssemblyDescription("WebSharper Utilities")>]
+    [<AssemblyFileVersion("0.1.0.0")>]
+    [<AssemblyProduct("ExtSharper")>]
+    [<AssemblyTitle("ExtSharper")>]
+    [<AssemblyVersion("0.1.4.1")>]
+
+    do ()
---
+++ ExtSharper/Random.fs
@@ -0,0 +1,28 @@
+﻿namespace ExtSharper
+
+open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.EcmaScript
+
+type Random =
+        
+    /// Returns a random positive number.
+    [<JavaScript>]
+    static member Next () =
+        Math.Random () * 2147483648.
+        |> Math.Floor
+
+    /// Returns a random positive number that is less than the specified max value.
+    [<JavaScript>]
+    static member Next maxValue =
+        Math.Random () * (maxValue + 1 |> float)
+        |> Math.Floor
+
+    /// Returns a random positive number in the specified range.
+    [<JavaScript>]
+    static member Next (minValue, maxValue) =
+        Math.Random () * (maxValue - minValue + 1 |> float) + float minValue
+        |> Math.Floor
+
+    /// Returns a random number between 0.0 and 1.0.
+    [<JavaScript>]
+    static member NextDouble () = Math.Random ()
