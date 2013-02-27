62f3e788d9de23bad9d26cbd93e253e6e7867811
Taha Hachana <tahahachana@live.com>
2013-02-27 10:38:21 +0100

    rename HtmlDocument module

---
+++ ExtSharper/JQuery.fs
@@ -0,0 +1,28 @@
+﻿namespace ExtSharper.Client
+
+open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.JQuery
+
+module JQuery =
+
+    [<JavaScript>]
+    let GetAttribute (selector : string, attributeName) = JQuery.Of(selector).Attr(attributeName)
+
+    [<JavaScript>]
+    let GetText (selector : string) = JQuery.Of(selector).Text()
+
+    [<JavaScript>]
+    let AddClass (selector : string, className : string) = JQuery.Of(selector).AddClass(className).Ignore
+
+    [<JavaScript>]
+    let RemoveClass (selector : string, className) = JQuery.Of(selector).RemoveClass(className).Ignore
+
+    [<JavaScript>]
+    let SetAttribute (selector: string, attributeName, attributeValue) = JQuery.Of(selector).Attr(attributeName, attributeValue).Ignore
+
+    [<JavaScript>]
+    let SetText (selector : string, newText) = JQuery.Of(selector).Text(newText).Ignore
+
+    
+
+
