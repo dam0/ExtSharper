448921506be4f8454d892729243e88e4aa3705e3
Taha Hachana <tahahachana@live.com>
2013-02-28 09:57:59 +0100

    delete JQuery module

--- ExtSharper/AssemblyInfo.fs
+++ ExtSharper/AssemblyInfo.fs
@@ -9,6 +9,6 @@ module AssemblyInfo =
     [<AssemblyFileVersion("0.1.0.0")>]
     [<AssemblyProduct("ExtSharper")>]
     [<AssemblyTitle("ExtSharper")>]
-    [<AssemblyVersion("0.1.10.5")>]
+    [<AssemblyVersion("0.1.11.6")>]
 
     do ()
